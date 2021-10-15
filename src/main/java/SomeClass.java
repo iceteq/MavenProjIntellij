8dbdc4c95449d3c961b73a75e921d1160f0a6639
iceteq <icetake@live.se>
2021-10-15 04:43:34 +0200

    Initial commit

---
+++ src/main/java/SomeClass.java
@@ -0,0 +1,14 @@
+public class SomeClass {
+
+
+
+    int val;
+
+    public SomeClass(int someValue) {
+        this.val = someValue;
+    }
+
+    public int getVal() {
+        return val;
+    }
+}
