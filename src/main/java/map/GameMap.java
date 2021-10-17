a278e07a464dc804ed2f1481a42811c027d9ffe5
PhilipSjun <56636091+PhilipSjun@users.noreply.github.com>
2021-10-17 19:00:05 +0200

    Added functionality to MapTile and GameMap
    Added toString() and equals() methods to MapTile. Changed scope on some methods in MapTile and moved some values to being constants.
    Added ability to create a default map, change the tiles in a default map and get tiles to GameMap.

--- src/main/java/map/GameMap.java
+++ src/main/java/map/GameMap.java
@@ -1,6 +1,62 @@
 package map;
 public class GameMap {
 	
+	private static final int MIN_INDEX = 0;
+	private static final int MAX_INDEX = 200;
 	
+	private MapTile[][] map;
 	
+	public GameMap(int x, int y) {
+		if(!(validateIndex(x) && validateIndex(y))) {
+			throw new IllegalArgumentException();
+		}
+		this.map = new MapTile[x][y];
+		setDefaultMap();
+	}
+	
+	protected static final int getMIN_INDEX() { return MIN_INDEX; }
+	protected static final int getMAX_INDEX() { return MAX_INDEX; }
+	
+	private boolean validateIndex(int i) {
+		if(i < MIN_INDEX | i > MAX_INDEX) {
+			return false;
+		}
+		return true;
+	}
+	
+	private void setTile(int x, int y) {
+		if(!(validateIndex(x) && validateIndex(y))) {
+			throw new IllegalArgumentException();
+		}
+		map[x][y] = new MapTile();
+	}
+	
+	public void setTile(int x, int y, int height) {
+		if(!(validateIndex(x) && validateIndex(y))) {
+			throw new IndexOutOfBoundsException();
+		}
+		map[x][y].setTile(height);
+	}
+	
+	public void setDefaultMap() {
+		if(!(map[0][0] == null)) {
+			throw new IllegalStateException();
+		}
+		for(int x = 0; x < map[0].length; x++) {
+			for(int y = 0; y < map.length; y++) {
+				setTile(x,y);
+			}
+		}
+	}
+	
+	public MapTile getTile(int x, int y) {
+		if(!(validateIndex(x) && validateIndex(y))) {
+			throw new IndexOutOfBoundsException();
+		}
+		return map[x][y];
+	}
+	
+	protected MapTile[][] getMap() {
+		return map;
+	}
 }
--- src/main/java/map/MapTile.java
+++ src/main/java/map/MapTile.java
@@ -1,32 +1,54 @@
 package map;
 public class MapTile {
 	
-	private static final int MINHEIGHT = 0;
-	private static final int MAXHEIGHT = 200;
-	private static final int OCEANMAXHEIGHT = 40;
-	private static final int PLAINSMAXHEIGHT = 110;
-	private static final int MOUNTAINMAXHEIGHT = MAXHEIGHT;
+	private static final int MIN_HEIGHT = 0;
+	private static final int MAX_HEIGHT = 200;
+	private static final int OCEAN_MAX_HEIGHT = 40;
+	private static final int PLAINS_MAX_HEIGHT = 110;
+	private static final int MOUNTAIN_MAX_HEIGHT = MAX_HEIGHT;
 	
 	private int height;
 	private enum Biome {
-		OCEAN("ocean", "swimming"),
-		PLAINS("plains", "walking"),
-		MOUNTAIN("mountain", "flight");
+		OCEAN("ocean", "swimming", "˜"),
+		PLAINS("plains", "walking", "."),
+		MOUNTAIN("mountain", "flight", "∧"),
+		DEFAULT("none", "none", "");
 		
 		private String name;
 		private String accessibility;
-		private Biome(String name, String accessibility) {
+		private String symbol;
+		private Biome(String name, String accessibility, String symbol) {
 			this.name = name;
 			this.accessibility = accessibility;
+			this.symbol = symbol;
 		}
 	}
 	private Biome biome;
 	
-	public MapTile(int i) {
-		
-		switch ((MINHEIGHT <= i && i <= OCEANMAXHEIGHT) ? 0 : //
-			(OCEANMAXHEIGHT < i && i <= PLAINSMAXHEIGHT) ? 1 :
-				(PLAINSMAXHEIGHT < i && i <= MOUNTAINMAXHEIGHT) ? 2 : 3) {
+	protected MapTile() {
+		this.biome = Biome.DEFAULT;
+		this.height = 0;
+	}
+	
+	protected MapTile(int i) {
+		this.biome = Biome.DEFAULT;
+		this.height = 0;
+		setTile(i);
+	}
+	
+	protected static int getMIN_HEIGHT() { return MIN_HEIGHT; }
+	protected static int getMAX_HEIGHT() { return MAX_HEIGHT; }
+	protected static int getOCEAN_MAX_HEIGHT() { return OCEAN_MAX_HEIGHT; }
+	protected static int getPLAINS_MAX_HEIGHT() { return PLAINS_MAX_HEIGHT; }
+	protected static int getMOUNTAIN_MAX_HEIGHT() { return MOUNTAIN_MAX_HEIGHT; }
+	
+	protected void setTile(int i) {
+		if(!(biome == Biome.DEFAULT)) {
+			throw new IllegalStateException();
+		}
+		switch ((MIN_HEIGHT <= i && i <= OCEAN_MAX_HEIGHT) ? 0 : //
+			(OCEAN_MAX_HEIGHT < i && i <= PLAINS_MAX_HEIGHT) ? 1 :
+				(PLAINS_MAX_HEIGHT < i && i <= MOUNTAIN_MAX_HEIGHT) ? 2 : 3) {
 			
 		case 0:
 			this.biome = Biome.OCEAN;
@@ -55,4 +77,22 @@ public class MapTile {
 		return biome.name;
 	}
 	
+	@Override
+	public String toString() {
+		return biome.symbol;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		if (o == this) {
+			return true;
+		}
+		if (!(o instanceof MapTile)) {
+			return false;
+		}
+		MapTile comparand = (MapTile) o;
+		return height == comparand.height
+				&& biome == comparand.biome;
+	}
 }
