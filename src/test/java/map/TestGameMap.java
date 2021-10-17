0bf60eb57660451c38b580afa24794c095493edb
PhilipSjun <56636091+PhilipSjun@users.noreply.github.com>
2021-10-17 18:56:44 +0200

    Unit tests for GameMap and MapTile
    Added more unit tests for MapTile, and added unit tests for GameMap.

--- src/test/java/map/TestGameMap.java
+++ src/test/java/map/TestGameMap.java
@@ -1,6 +1,152 @@
 package map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 public class TestGameMap {
 	
+	private static final int HEIGHT = 100;
+	private static final int WIDTH = 100;
+	private static final int INDEX_TOO_LOW = -1;
+	private static final int INDEX_HEIGHT_TOO_HIGH = HEIGHT+1;
+	private static final int INDEX_WIDTH_TOO_HIGH = WIDTH+1;
 	
+	private Random random = new Random();
+		
+	@Test
+	public void testDefaultGeneration() {
+		MapTile defaultTile = new MapTile();
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		for(int x = 0; x < gameMap.getMap()[0].length; x++) {
+			for(int y = 0; y < gameMap.getMap().length; y++) {
+				assertEquals(defaultTile, gameMap.getTile(x,y));
+			}
+		}
+	}
 	
+	@Test
+	public void testGetDefaultTile() {
+		MapTile defaultTile = new MapTile();
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertEquals(defaultTile, gameMap.getTile(random.nextInt(WIDTH+1), random.nextInt(HEIGHT+1)));
+	}
+	
+	@Test
+	public void testSetDefaultMapOnExistingMap() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		for(int x = 0; x < gameMap.getMap()[0].length; x++) {
+			for(int y = 0; y < gameMap.getMap().length; y++) {
+				gameMap.setTile(x,y,random.nextInt(MapTile.getMAX_HEIGHT()+1));
+			}
+		}
+		assertThrows(IllegalStateException.class, () -> {
+			gameMap.setDefaultMap();
+		});
+	}
+	
+	@Test
+	public void testSetTiles() {
+		MapTile defaultTile = new MapTile();
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		for(int x = 0; x < gameMap.getMap()[0].length; x++) {
+			for(int y = 0; y < gameMap.getMap().length; y++) {
+				gameMap.setTile(x,y,random.nextInt(MapTile.getMAX_HEIGHT()+1));
+				assertFalse(defaultTile.equals(gameMap.getTile(x,y)));
+			}
+		}
+	}
+	
+	@Test
+	public void testMakeMapXTooSmall() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			GameMap map = new GameMap(GameMap.getMIN_INDEX()-1,0);
+		});
+	}
+	
+	@Test
+	public void testMakeMapYTooSmall() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			GameMap map = new GameMap(0,GameMap.getMIN_INDEX()-1);
+		});
+	}
+	
+	@Test
+	public void testMakeMapXTooLarge() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			GameMap map = new GameMap(GameMap.getMAX_INDEX()+1,0);
+		});
+	}
+	
+	@Test
+	public void testMakeMapYTooLarge() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			GameMap map = new GameMap(0,GameMap.getMAX_INDEX()+1);
+		});
+	}
+	
+	@Test
+	public void testSetTileXIndexTooLow() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.setTile(INDEX_TOO_LOW,0,MapTile.getMAX_HEIGHT());
+		});
+	}
+	
+	@Test
+	public void testSetTileYIndexTooLow() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.setTile(0,INDEX_TOO_LOW,MapTile.getMAX_HEIGHT());
+		});
+	}
+	
+	@Test
+	public void testSetTileXIndexTooHigh() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.setTile(INDEX_WIDTH_TOO_HIGH,0,MapTile.getMAX_HEIGHT());
+		});
+	}
+	
+	@Test
+	public void testSetTileYIndexTooHigh() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.setTile(0,INDEX_HEIGHT_TOO_HIGH,MapTile.getMAX_HEIGHT());
+		});
+	}
+	
+	@Test
+	public void testGetTileXIndexTooLow() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.getTile(INDEX_TOO_LOW,0);
+			});
+	}
+	
+	public void testGetTileYIndexTooLow() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.getTile(0,INDEX_TOO_LOW);
+			});
+	}
+	
+	public void testGetTileXIndexTooHigh() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.getTile(INDEX_WIDTH_TOO_HIGH,0);
+			});
+	}
+	
+	public void testGetTileYIndexTooHigh() {
+		GameMap gameMap = new GameMap(HEIGHT,WIDTH);
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			gameMap.getTile(0,INDEX_HEIGHT_TOO_HIGH);
+			});
+	}
 }
--- src/test/java/map/TestMapTile.java
+++ src/test/java/map/TestMapTile.java
@@ -6,58 +6,172 @@ import map.MapTile;
 
 public class TestMapTile {
 	
+	private static final int TOO_LOW = MapTile.getMIN_HEIGHT()-1;
+	private static final int TOO_HIGH = MapTile.getMAX_HEIGHT()+1;
+	
+	private MapTile mountainTile = new MapTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
+	private MapTile plainsTile = new MapTile(MapTile.getPLAINS_MAX_HEIGHT());
+	private MapTile oceanTile = new MapTile(MapTile.getOCEAN_MAX_HEIGHT());
+	private MapTile defaultTile = new MapTile();
+	
 	@Test
 	public void testHeightTooLow() {
 		assertThrows(IllegalArgumentException.class, () -> {
-			MapTile tile = new MapTile(-1);});
+			MapTile tile = new MapTile(TOO_LOW);});
 	}
 	
 	@Test
 	public void testHeightTooHigh() {
 		assertThrows(IllegalArgumentException.class, () -> {
-			MapTile tile = new MapTile(201);});
+			MapTile tile = new MapTile(TOO_HIGH);});
+	}
+	
+	@Test
+	public void testHeightDefaultTile() {
+		assertEquals(MapTile.getMIN_HEIGHT(), defaultTile.getHeight());
+	}
+	
+	@Test
+	public void testAccessibilityDefaultTile() {
+		assertEquals("none", defaultTile.getAccessibility());
+	}
+	
+	@Test
+	public void testBiomeDefaultTile() {
+		assertEquals("none", defaultTile.getBiome());
+	}
+	
+	@Test
+	public void testSymbolDefaultTile() {
+		assertEquals("", defaultTile.toString());
 	}
 	
 	@Test
 	public void testGetHeight() {
-		MapTile tile = new MapTile(50);
-		assertEquals(50, tile.getHeight());
+		assertEquals(MapTile.getPLAINS_MAX_HEIGHT(), plainsTile.getHeight());
 	}
 	
 	@Test
 	public void testGetAccessibilityFlight() {
-		MapTile tile = new MapTile(180);
-		assertEquals("flight", tile.getAccessibility());
+		assertEquals("flight", mountainTile.getAccessibility());
 	}
 	
 	@Test
 	public void testGetAccessibilitySwim() {
-		MapTile tile = new MapTile(20);
-		assertEquals("swimming", tile.getAccessibility());
+		assertEquals("swimming", oceanTile.getAccessibility());
 	}
 	
 	@Test
 	public void testGetAccessibilityWalk() {
-		MapTile tile = new MapTile(50);
-		assertEquals("walking", tile.getAccessibility());
+		assertEquals("walking", plainsTile.getAccessibility());
 	}
 	
 	@Test
 	public void testGetBiomeMountain() {
-		MapTile tile = new MapTile(180);
-		assertEquals("mountain", tile.getBiome());
+		assertEquals("mountain", mountainTile.getBiome());
 	}
 	
 	@Test
 	public void testGetBiomeOcean() {
-		MapTile tile = new MapTile(20);
-		assertEquals("ocean", tile.getBiome());
+		assertEquals("ocean", oceanTile.getBiome());
 	}
 	
 	@Test
 	public void testGetBiomePlains() {
-		MapTile tile = new MapTile(50);
-		assertEquals("plains", tile.getBiome());
+		assertEquals("plains", plainsTile.getBiome());
 	}
-
+	
+	@Test
+	public void testToStringMountain() {
+		assertEquals("∧", mountainTile.toString());
+	}
+	
+	@Test
+	public void testToStringOcean() {
+		assertEquals("˜", oceanTile.toString());
+	}
+	
+	@Test
+	public void testToStringPlains() {
+		assertEquals(".", plainsTile.toString());
+	}
+	
+	@Test
+	public void testEqualsMountain() {
+		MapTile tile = new MapTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
+		assertTrue(tile.equals(mountainTile));
+	}
+	
+	@Test
+	public void testNotEqualsMountain() {
+		MapTile tile = new MapTile(MapTile.getPLAINS_MAX_HEIGHT());
+		assertFalse(tile.equals(mountainTile));
+	}
+	
+	@Test
+	public void testEqualsPlains() {
+		MapTile tile = new MapTile(MapTile.getPLAINS_MAX_HEIGHT());
+		assertTrue(tile.equals(plainsTile));
+	}
+	
+	@Test
+	public void testNotEqualsPlains() {
+		MapTile tile = new MapTile(MapTile.getOCEAN_MAX_HEIGHT());
+		assertFalse(tile.equals(plainsTile));
+	}
+	
+	@Test
+	public void testEqualsOcean() {
+		MapTile tile = new MapTile(MapTile.getOCEAN_MAX_HEIGHT());
+		assertTrue(tile.equals(oceanTile));
+	}
+	
+	@Test
+	public void testNotEqualsOcean() {
+		MapTile tile = new MapTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
+		assertFalse(tile.equals(oceanTile));
+	}
+	
+	@Test
+	public void testSetMountain() {
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
+		assertTrue(tile.equals(mountainTile));
+	}
+	
+	@Test
+	public void testSetPlains() {
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getPLAINS_MAX_HEIGHT());
+		assertTrue(tile.equals(plainsTile));
+	}
+	
+	@Test
+	public void testSetOcean() {
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getOCEAN_MAX_HEIGHT());
+		assertTrue(tile.equals(oceanTile));
+	}
+	
+	@Test
+	public void testSetHeightTooHigh() {
+		MapTile tile = new MapTile();
+		assertThrows(IllegalArgumentException.class, () -> {
+			tile.setTile(TOO_LOW);});
+	}
+	
+	@Test
+	public void testSetHeightTooLow() {
+		MapTile tile = new MapTile();
+		assertThrows(IllegalArgumentException.class, () -> {
+			tile.setTile(TOO_HIGH);});
+	}
+	
+	@Test
+	public void testSetAlreadyAssigned() {
+		MapTile tile = new MapTile(MapTile.getMAX_HEIGHT());
+		assertThrows(IllegalStateException.class, () -> {
+			tile.setTile(MapTile.getMIN_HEIGHT());});
+	}
+	
 }
