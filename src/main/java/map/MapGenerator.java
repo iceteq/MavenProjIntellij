04f894a5b6582ddce0aab063c10496d60d3b5895
PhilipSjun <56636091+PhilipSjun@users.noreply.github.com>
2021-10-26 08:31:07 +0200

    Update MapGenerator.java
    Added comments and finished the generation of the map. Quite barebones, spent too long on it and don't have time to do the things I wanted with it.

--- src/main/java/map/MapGenerator.java
+++ src/main/java/map/MapGenerator.java
@@ -1,5 +1,41 @@
 package map;
 
-public class MapGenerator {
+import java.util.Random;
 
+import de.articdive.jnoise.JNoise;
+import de.articdive.jnoise.interpolation.InterpolationType;
+
+public abstract class MapGenerator {
+	
+	//Create a noise map using the provided Long seed
+	//Noise map is generated using Perlin's algorithm, with the interpolation set to linear.
+	//Should only need to be used when generating a map.
+	private JNoise generateNoise(Long seed) {
+		return JNoise.newBuilder().perlin().setInterpolation(InterpolationType.LINEAR).setSeed(seed).build();
+	}
+	
+	//Will create a new map based on a set seed, and then using the noise map
+	//it will assign the height of each tile of the map.
+	//This will then result in a map with different biomes.
+	//
+	//With Perlin noise, the noise value at whole integers (e.g 1,1)
+	//is always equal to 0, therefore the x&y values in the for loop
+	//are divided by 30.
+	public GameMap generateMap(int width, int height, Long seed) {
+		JNoise noise = generateNoise(seed);
+		GameMap map = new GameMap(height, width);
+		for (int x = GameMap.getMIN_DIMENSION(); x < width; x++) {
+			for (int y = GameMap.getMIN_DIMENSION(); y < height; y++) {
+				map.setTile(x, y, (int)((noise.getNoise(((double)x)/30, ((double)y)/30)+1)*100));
+			}
+		}
+		return map;
+	}
+	
+	//Will create a new map based on a random seed by calling
+	//generateMap(int,int,seed);
+	public GameMap generateMap(int width, int height) {
+		Random random = new Random();
+		return generateMap(width,height,random.nextLong());
+	}
 }
