73e5c0b6f19d47ceb0c5b1ac8c0779a62e204556
PhilipSjun <56636091+PhilipSjun@users.noreply.github.com>
2021-10-26 11:02:58 +0200

    Changed style format of map package.
    Changed the style of the classes in the map package to comply with the CheckStyle format. Added Javadoc compliant documentation comments.

--- src/test/java/map/TestMapGenerator.java
+++ src/test/java/map/TestMapGenerator.java
@@ -1,67 +1,80 @@
 package map;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import de.articdive.jnoise.JNoise;
 import de.articdive.jnoise.interpolation.InterpolationType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+/**
+ * TestMapGenerator.java - test class for MapGenerator.
+ *
+ *@author Philip Sjunnesson
+ *@see MapGenerator
+ */
 public class TestMapGenerator {
-	
-	private static final GameMap gameMap = new GameMap(2,2);
-	private static final MapGenerator generator = new MapGenerator() {};
-	private static final JNoise noise = JNoise.newBuilder().perlin().setInterpolation(InterpolationType.LINEAR).setSeed(500l).build();
-	
-	@BeforeAll
-	public static void init() {
-		for (int x = GameMap.getMIN_DIMENSION(); x < gameMap.getWidth(); x++) {
-			for (int y = GameMap.getMIN_DIMENSION(); y < gameMap.getHeight(); y++) {
-				gameMap.setTile(x, y, (int)((noise.getNoise(((double)x)/30, ((double)y)/30)+1)*100));
-			}
-		}
-	}
-	
-	@Test
-	public void testGenerateMap() {
-		assertArrayEquals(gameMap.getMap(), generator.generateMap(2,2,500l).getMap());
-	}
-	
-	@Test
-	public void testGenerateRandomMap() {
-		GameMap map = generator.generateMap(100, 100);
-		MapTile defaultTile = new MapTile();
-		assertFalse(map.getTile(0, 0).equals(defaultTile));
-	}
-	
-	@Test
-	public void testGenerateMapNotTooSmall() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			generator.generateMap(GameMap.getMIN_DIMENSION()-1, GameMap.getMIN_DIMENSION()-1, 500l);
-		});
-	}
-	@Test
-	public void testGenerateMapNotTooLarge() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			generator.generateMap(GameMap.getMAX_DIMENSION()+1, GameMap.getMAX_DIMENSION()+1, 500l);
-		});
-	}
-	
-	@Test
-	public void testGenerateRandomMapNotTooSmall() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			generator.generateMap(GameMap.getMIN_DIMENSION()-1, GameMap.getMIN_DIMENSION()-1);
-		});
-	}
-	@Test
-	public void testGenerateRandomMapNotTooLarge() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			generator.generateMap(GameMap.getMAX_DIMENSION()+1, GameMap.getMAX_DIMENSION()+1);
-		});
-	}
+
+  private static final GameMap gameMap = new GameMap(2, 2);
+  private static final MapGenerator generator = new MapGenerator() {};
+  private static final JNoise noise = JNoise.newBuilder()
+      .perlin()
+      .setInterpolation(InterpolationType.LINEAR)
+      .setSeed(500L)
+      .build();
+
+  /**
+   * Initialises the comparand map to compare the MapGenerator methods against.
+   */
+  @BeforeAll
+  public static void init() {
+    for (int x = GameMap.getMin_Dimension(); x < gameMap.getWidth(); x++) {
+      for (int y = GameMap.getMin_Dimension(); y < gameMap.getHeight(); y++) {
+        gameMap.setTile(x, y, 
+            (int) ((noise.getNoise(((double) x) / 30, ((double) y) / 30) + 1) * 100));
+      }
+    }
+  }
+
+  @Test
+  public void testGenerateMap() {
+    assertArrayEquals(gameMap.getMap(), generator.generateMap(2, 2, 500L).getMap());
+  }
+
+  @Test
+  public void testGenerateRandomMap() {
+    GameMap map = generator.generateMap(100, 100);
+    MapTile defaultTile = new MapTile();
+    assertFalse(map.getTile(0, 0).equals(defaultTile));
+  }
+
+  @Test
+  public void testGenerateMapNotTooSmall() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      generator.generateMap(GameMap.getMin_Dimension() - 1, GameMap.getMin_Dimension() - 1, 500L);
+    });
+  }
+  
+  @Test
+  public void testGenerateMapNotTooLarge() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      generator.generateMap(GameMap.getMax_Dimension() + 1, GameMap.getMax_Dimension() + 1, 500L);
+    });
+  }
+
+  @Test
+  public void testGenerateRandomMapNotTooSmall() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      generator.generateMap(GameMap.getMin_Dimension() - 1, GameMap.getMin_Dimension() - 1);
+    });
+  }
+  
+  @Test
+  public void testGenerateRandomMapNotTooLarge() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      generator.generateMap(GameMap.getMax_Dimension() + 1, GameMap.getMax_Dimension() + 1);
+    });
+  }
 }
