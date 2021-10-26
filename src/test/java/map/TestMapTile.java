049ac401d1c013ba4495b8631b80d580f29dec11
PhilipSjun <56636091+PhilipSjun@users.noreply.github.com>
2021-10-26 08:23:50 +0200

    Added tests and refactoring to TestMapTile
    Added tests to achieve 100% coverage and for the new function isOccupied().

--- src/test/java/map/TestMapTile.java
+++ src/test/java/map/TestMapTile.java
@@ -1,5 +1,7 @@
 package map;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import map.MapTile;
@@ -9,21 +11,30 @@ public class TestMapTile {
 	private static final int TOO_LOW = MapTile.getMIN_HEIGHT()-1;
 	private static final int TOO_HIGH = MapTile.getMAX_HEIGHT()+1;
 	
-	private MapTile mountainTile = new MapTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
-	private MapTile plainsTile = new MapTile(MapTile.getPLAINS_MAX_HEIGHT());
-	private MapTile oceanTile = new MapTile(MapTile.getOCEAN_MAX_HEIGHT());
-	private MapTile defaultTile = new MapTile();
+	private static final MapTile mountainTile = new MapTile();
+	private static final MapTile plainsTile = new MapTile();
+	private static final MapTile oceanTile = new MapTile();
+	private static final MapTile defaultTile = new MapTile();
+	
+	@BeforeAll
+	public static void init() {
+		mountainTile.setTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
+		plainsTile.setTile(MapTile.getPLAINS_MAX_HEIGHT());
+		oceanTile.setTile(MapTile.getOCEAN_MAX_HEIGHT());
+	}
 	
 	@Test
 	public void testHeightTooLow() {
+		MapTile tile = new MapTile();
 		assertThrows(IllegalArgumentException.class, () -> {
-			MapTile tile = new MapTile(TOO_LOW);});
+			tile.setTile(TOO_LOW);});
 	}
 	
 	@Test
 	public void testHeightTooHigh() {
+		MapTile tile = new MapTile();
 		assertThrows(IllegalArgumentException.class, () -> {
-			MapTile tile = new MapTile(TOO_HIGH);});
+			tile.setTile(TOO_HIGH);});
 	}
 	
 	@Test
@@ -43,7 +54,7 @@ public class TestMapTile {
 	
 	@Test
 	public void testSymbolDefaultTile() {
-		assertEquals("", defaultTile.toString());
+		assertEquals("", defaultTile.getSymbol());
 	}
 	
 	@Test
@@ -98,52 +109,68 @@ public class TestMapTile {
 	
 	@Test
 	public void testToStringMountain() {
-		assertEquals("Height: 200, Biome: mountain, Accessibility: flight, Symbol: ∧", mountainTile.toString());
+		assertEquals("Height: " + MapTile.getMOUNTAIN_MAX_HEIGHT() + ", Biome: mountain, Accessibility: flight, Symbol: ∧", mountainTile.toString());
 	}
 	
 	@Test
 	public void testToStringOcean() {
-		assertEquals("Height: 40, Biome: ocean, Accessibility: swimming, Symbol: ˜", mountainTile.toString());
+		assertEquals("Height: " + MapTile.getOCEAN_MAX_HEIGHT() + ", Biome: ocean, Accessibility: swimming, Symbol: ˜", oceanTile.toString());
 	}
 	
 	@Test
 	public void testToStringPlains() {
-		assertEquals("Height: 110, Biome: plains, Accessibility: walking, Symbol: .", mountainTile.toString());
+		assertEquals("Height: " + MapTile.getPLAINS_MAX_HEIGHT() + ", Biome: plains, Accessibility: walking, Symbol: .", plainsTile.toString());
+	}
+	
+	@Test
+	public void testEqualsItself() {
+		assertTrue(mountainTile.equals(mountainTile));
+	}
+	
+	@Test
+	public void testEqualsAnotherObject() {
+		assertFalse(mountainTile.equals(54));
 	}
 	
 	@Test
 	public void testEqualsMountain() {
-		MapTile tile = new MapTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
 		assertTrue(tile.equals(mountainTile));
 	}
 	
 	@Test
 	public void testNotEqualsMountain() {
-		MapTile tile = new MapTile(MapTile.getPLAINS_MAX_HEIGHT());
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getPLAINS_MAX_HEIGHT());
 		assertFalse(tile.equals(mountainTile));
 	}
 	
 	@Test
 	public void testEqualsPlains() {
-		MapTile tile = new MapTile(MapTile.getPLAINS_MAX_HEIGHT());
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getPLAINS_MAX_HEIGHT());
 		assertTrue(tile.equals(plainsTile));
 	}
 	
 	@Test
 	public void testNotEqualsPlains() {
-		MapTile tile = new MapTile(MapTile.getOCEAN_MAX_HEIGHT());
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getOCEAN_MAX_HEIGHT());
 		assertFalse(tile.equals(plainsTile));
 	}
 	
 	@Test
 	public void testEqualsOcean() {
-		MapTile tile = new MapTile(MapTile.getOCEAN_MAX_HEIGHT());
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getOCEAN_MAX_HEIGHT());
 		assertTrue(tile.equals(oceanTile));
 	}
 	
 	@Test
 	public void testNotEqualsOcean() {
-		MapTile tile = new MapTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getMOUNTAIN_MAX_HEIGHT());
 		assertFalse(tile.equals(oceanTile));
 	}
 	
@@ -184,9 +211,55 @@ public class TestMapTile {
 	
 	@Test
 	public void testSetAlreadyAssigned() {
-		MapTile tile = new MapTile(MapTile.getMAX_HEIGHT());
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getMAX_HEIGHT());
 		assertThrows(IllegalStateException.class, () -> {
 			tile.setTile(MapTile.getMIN_HEIGHT());});
+	}
+	
+	@Test
+	public void testSetOccupied() {
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getPLAINS_MAX_HEIGHT());
+		tile.setOccupied(true);
+		assertTrue(tile.isOccupied());
+	}
+	
+	@Test
+	public void testGetDefaultOccupied() {
+		MapTile tile = new MapTile();
+		assertFalse(tile.isOccupied());
+	}
+	
+	@Test
+	public void testSetOccupiedAlreadyOccupied() {
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getPLAINS_MAX_HEIGHT());
+		tile.setOccupied(true);
+		assertThrows(IllegalArgumentException.class, () -> {
+			tile.setOccupied(true);});
+	}
+	
+	@Test
+	public void testSetVacantAlreadyVacant() {
+		MapTile tile = new MapTile();
+		tile.setTile(MapTile.getPLAINS_MAX_HEIGHT());
+		assertThrows(IllegalArgumentException.class, () -> {
+			tile.setOccupied(false);});
+	}
+	
+	@Test
+	public void testSetOccupiedOnDefaultTile() {
+		MapTile tile = new MapTile();
+		assertThrows(IllegalStateException.class, () -> {
+			tile.setOccupied(true);});
+	}
+	
+	@Test
+	public void testSetVacantOnDefaultTile() {
+		MapTile tile = new MapTile();
+		assertThrows(IllegalStateException.class, () -> {
+			tile.setOccupied(false);});
 	}
 	
 }
